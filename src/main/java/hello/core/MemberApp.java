acb1f4bed4082e6de37fdc1c1e3225957df7b3e0
fDevJc <fDevJc@gmail.com>
2022-04-21 10:56:28 +0900

    AppConfig를 통하여 관심사의 분리

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -8,7 +8,8 @@ import hello.core.member.MemberServiceImpl;
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         Member member1 = new Member(1L, "Member1", Grade.VIP);
         memberService.join(member1);
 
