d39c3ee048507abf9076b1a9e6289ce38e4af92a
fDevJc <fDevJc@gmail.com>
2022-04-21 12:17:27 +0900

    스프링 적용

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,20 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        
+        //name은 메서드 이름으로 된다
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member1 = new Member(1L, "Member1", Grade.VIP);
         memberService.join(member1);
 
