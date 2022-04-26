8c8886668202edcce0ed5cfc527f5edc9774e9e1
fDevJc <fDevJc@gmail.com>
2022-04-26 12:50:11 +0900

    싱글톤 패턴과 프로토타입을 같이 쓸경우 문제점 테스트

---
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,93 @@
+package hello.core.scope;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class SingletonWithPrototypeTest1 {
+
+    @Test
+    void prototypeFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(
+            PrototypeBean.class);
+
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.add();
+        System.out.println(prototypeBean1.getCount());
+
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.add();
+        System.out.println(prototypeBean2.getCount());
+
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+        ac.close();
+    }
+
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(
+            SingletonBean.class, PrototypeBean.class);
+
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        singletonBean1.logic();
+        System.out.println("singletonBean1.getCount() = " + singletonBean1.getCount());
+
+        assertThat(singletonBean1.getCount()).isEqualTo(1);
+
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+        singletonBean2.logic();
+        System.out.println("singletonBean2.getCount() = " + singletonBean2.getCount());
+
+        assertThat(singletonBean1.getCount()).isEqualTo(2);
+        assertThat(singletonBean2.getCount()).isEqualTo(2);
+
+    }
+
+    @Scope("singleton")
+    static class SingletonBean {
+        private final PrototypeBean prototypeBean;
+
+        public SingletonBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+        
+        public void logic() {
+            prototypeBean.add();
+        }
+        
+        public int getCount() {
+            return prototypeBean.getCount();
+        }
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        private int count = 0;
+
+        public void add() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init" + this);
+        }
+
+        @PreDestroy
+        public void close() {
+            System.out.println("PrototypeBean.close");
+        }
+    }
+}
